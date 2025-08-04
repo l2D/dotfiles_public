645b00eb36694fb4da9fc682ca60e11614500d5b
l2D <20911264+l2D@users.noreply.github.com>
2025-08-04 23:23:12 +0700

    🔧 chore: updates setup script to use bash
    - Updates the setup script to use bash instead of zsh
    - Installs Homebrew using the recommended installation command
    - Executes brew bundle install and ohmyzsh install using bash

--- setup_my_new_device.sh
+++ setup_my_new_device.sh
@@ -1,10 +1,10 @@
-#!/bin/zsh
+#!/bin/bash
 
-# Install HomeBrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Brew install from BrewFile
-sh "brew bundle install"
+bash -c "brew bundle install"
 
-# Install ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# # Install ohmyzsh
+# bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
